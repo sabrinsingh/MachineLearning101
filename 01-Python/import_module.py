87f32fccf23503d60e703f2dd4daf1a84d371008
Sabrin Lal Singh <sabrinlalsingh@gmail.com>
2023-10-14 19:15:41 +0545

    Add files via upload

---
+++ 01-Python/import_module.py
@@ -0,0 +1,14 @@
+
+import module_to_find_area as ma
+
+l=2
+b=5
+r=3
+h=2
+base=5
+
+
+print('Aree of circle is ' + str(ma.area_of_circle(r)))
+print('Area of triangle is ' + str(ma.area_of_triangle(base,h)))
+print('Area of square is ' + str(ma.area_of_square(l,l)))
+print('Area of rectangle is ' + str(ma.area_of_square(l,b)))
---
+++ 01-Python/module_to_find_area.py
@@ -0,0 +1,9 @@
+import math
+def area_of_triangle(base,h):
+    return 1/2 * base * h
+
+def area_of_circle(r):
+    return math.pi * r **2
+
+def area_of_square(l,b):
+    return l*b
